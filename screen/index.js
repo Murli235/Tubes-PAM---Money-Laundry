7e9ed067d3e367fd8d44d4a8cd7abfed83428957
rendyprayoga <93699796+rendyprayoga@users.noreply.github.com>
2023-05-16 18:56:03 +0700

    index.js

---
+++ screen/index.js
@@ -0,0 +1,31 @@
+import splash from "./splash";
+import Home from "./Home";
+import login from "./login";
+import register from "./register";
+import History from "./history";
+import pengantaran from "./pengantaran";
+import Akun from "./Akun";
+import Menu from "./Menu";
+import Pemesanan from "./Pemesanan";
+import Satuan from "./Satuan";
+import Kiloan from "./Kiloan";
+import pembayaransatuan from "./pembayaransatuan";
+import pembayarankiloan from "./pembayarankiloan";
+import imagepic from "./imagepic";
+
+export {
+  Home,
+  splash,
+  login,
+  register,
+  History,
+  pengantaran,
+  Akun,
+  Menu,
+  Pemesanan,
+  Satuan,
+  Kiloan,
+  pembayaransatuan,
+  pembayarankiloan,
+  imagepic,
+};
